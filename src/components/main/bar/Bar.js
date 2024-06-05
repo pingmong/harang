86812991e44442d7672d6b1da0b29c4c86f32082
Hyehyun1001 <hye22327@naver.com>
2024-06-05 14:07:03 +0900

    이미지

--- src/components/main/bar/Bar.js
+++ src/components/main/bar/Bar.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from '../../../assets/images/logo.jpg';
-import menu from '../../../assets/icon/menu.jpg';
+import logo from '../../../assets/images/logo.svg';
+import menu from '../../../assets/icon/menu.svg';
 
 import './Bar.css';
 
