f5c7d6cf46e56c64bb438e9344081b8a8ba183ee
Hyehyun1001 <hye22327@naver.com>
2024-06-12 17:12:33 +0900

    design: food css 변경

--- src/components/main/food/Food.js
+++ src/components/main/food/Food.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './Food.css';
 import food from '../../../assets/icon/restaurant.svg'
 import add from '../../../assets/icon/add.svg'
+import right from "../../../assets/icon/right_arrow.svg";
+import left from "../../../assets/icon/left_arrow.svg"
 
 const Food = () => {
     return (
@@ -16,15 +18,36 @@ const Food = () => {
                 </button>
             </div>
             <div className="food-cards">
-                <div className="card food-card">
-                    <div className="card-header">오늘의 학식</div>
-                    <p className="meal-time">중식</p>
-                    <p className="meal-details">백미밥, 조개탕, 닭볶음탕, 백순대볶음*초장, 생크림단호박샐러드, 배추김치, 셀프계란후라이, 식빵&딸기잼, 구운파래김/숭늉</p>
+                <div className="card-food-card">
+                    <div className="food-card-header-left">
+                        <button className="arrow-button">
+                            <img src={left} alt="Left" className="arrow-icon"/>
+                        </button>
+                        오늘의 학식
+                    </div>
+                    <div className="meal-box">
+                        <p className="meal-time">중식</p>
+                        <p className="meal-details">백미밥, 조개탕, 닭볶음탕, 백순대볶음*초장, 생크림단호박샐러드, 배추김치, 셀프계란후라이, 식빵&딸기잼,
+                            구운파래김/숭늉</p>
+                    </div>
                 </div>
-                <div className="card food-card">
-                    <div className="card-header">학생생활관</div>
-                    <p className="meal-time">중식</p>
-                    <p className="meal-details">잔치국수, 추가밥, 왕만두&초간장, 단무지부추무침, 배추김치, 냉미숫가루</p>
+                <div className="card-food-card">
+                    <div className="food-card-header-right">
+                        학생생활관
+                        <button className="arrow-button">
+                            <img src={right} alt="Right" className="arrow-icon"/>
+                        </button>
+                    </div>
+                    <div className="meal-box">
+                        <p className="meal-time">중식</p>
+                        <p className="meal-details">
+                            잔치국수, 추가밥<br/>
+                            왕만두&초간장<br/>
+                            단무지부추무침<br/>
+                            배추김치<br/>
+                            &lt;냉미숫가루&gt;
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
