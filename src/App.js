b39f6f8e46d4b37e1ca29ea82a83e7a5eca5aa77
pingmong <kny7024@naver.com>
2024-06-22 18:34:33 +0900

    css 및 footer 수정

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
             <Food />
             <Schedule />
             <Notice />
-            {/*<Footer />*/}
+            <Footer />
         </div>
     );
 };
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -14,14 +14,15 @@ const Footer = () => {
                 <p className="footer-link">발전기금</p>
             </div>
             <div className="campus-info">
-                <p className="campus">유성덕명캠퍼스</p>
-                <p className="address">34518 대전광역시 유성구 동서대로 125(덕명동)</p>
-                <p className="contact">Tel. 042-821-1114</p>
-                <p className="contact">Fax. 042-825-5395</p>
-                <p className="campus">대덕산학융합캠퍼스</p>
-                <p className="address">34014 대전광역시 유성구 테크노1로 75(관평동)</p>
-                <p className="contact">Tel. 042-939-4701</p>
-                <p className="contact">Fax. 042-939-4705</p>
+                {/*<p className="campus">유성덕명캠퍼스</p>*/}
+                {/*<p className="address">34518 대전광역시 유성구 동서대로 125(덕명동)</p>*/}
+                {/*<p className="contact">Tel. 042-821-1114</p>*/}
+                {/*<p className="contact">Fax. 042-825-5395</p>*/}
+                {/*<p className="campus">대덕산학융합캠퍼스</p>*/}
+                {/*<p className="address">34014 대전광역시 유성구 테크노1로 75(관평동)</p>*/}
+                {/*<p className="contact">Tel. 042-939-4701</p>*/}
+                {/*<p className="contact">Fax. 042-939-4705</p>*/}
+
             </div>
         </div>
     );
